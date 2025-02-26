5aa765b3aeb6f516feca1598f38b671a7c2d1d3a
neeraj10122004 <119481961+neeraj10122004@users.noreply.github.com>
2025-02-26 12:09:13 +0000

    changes

--- api/index.js
+++ api/index.js
@@ -3,28 +3,37 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 dotenv.config();
 const jwtsecret = process.env.SECRET_KEY;
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL);
+
 const app = express();
 
-app.use(json)
+app.use(express.json());
+app.use(cors({ credentials: true, origin: '*' }));
+app.use(cookieParser());
 
-app.post("/register",async (req,res)=>{
-    const {username,password} = req.body;
-    var user;
-    try{
-    user = await User.create({username,password});
+app.post("/register", async (req, res) => {
+    console.log("register");
+    const { username, password } = req.body;
+    
+    try {
+        const user = await User.create({ username, password });
+        jwt.sign({ userId: user._id }, jwtsecret, {}, (err, token) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: "JWT Error" });
+            }
+            res.cookie('token', token, { httpOnly: true }).status(201).json({ message: 'User registered' });
+        });
+    } catch (e) {
+        return res.status(500).json({ error: 'Unique error' });
     }
-    catch(e){
-        res.status(401).json('unique error');
-    }
-    jwt.sign({userId:user._id},jwtsecret,(err,token)=>{
-        if(err) throw err;
-        res.cookie('token',token).status(201).json('ok');
-    });
 });
 
-app.listen(process.env.PORT,()=>{
-    console.log("listening")
+app.listen(process.env.PORT, () => {
+    console.log("Server listening on port", process.env.PORT);
 });
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,31 +1,47 @@
-import React from 'react'
-import { useState } from 'react'
-
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
-  const {username,setusername} = useState("");
-  const [password,setpassword] = useState("");
-  const register = async () =>{
-    const response = await axios.post('/register',{username,password})
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async () => {
+    try {
+      const response = await axios.post('/register', { username, password });
+      console.log(response.data);
+    } catch (error) {
+      console.error("Registration failed", error);
+    }
   };
+
   return (
     <div className='bg-blue-100 h-screen flex flex-col justify-center items-center'>
-      <form onSubmit={register} >
+      <form onSubmit={(e) => { e.preventDefault(); register(); }}>
+        <input 
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type="text" 
+          placeholder="Username" 
+          className="border-4 border-blue-100 p-1 block rounded-xl placeholder:text-center"
+        />
 
-        <input value={username}
-         onChange={(e)=>{setusername(e.target.value)}} type="text" name="username" id="username" placeholder='username' 
-         className=" border-4 border-blue-100 p-1 block black rounded-xl placeholder:text-center " />
+        <input 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password" 
+          placeholder="Password" 
+          className="border-4 border-blue-100 p-1 block rounded-xl placeholder:text-center"
+        />
 
-
-        <input value={password} onChange={(e)=>{setpassword(e.target.value)}} type="password" name="password" id="password" placeholder="password" 
-        className=" border-4 border-blue-100 p-1 block black rounded-xl placeholder:text-center " />
-
-
-        <button className=' border-4 border-blue-100 p-1 w-full flex items-center justify-center bg-blue-400 rounded-full ' type="submit"><div className="">Sign Up</div></button>
-
+        <button 
+          className='border-4 border-blue-100 p-1 w-full flex items-center justify-center bg-blue-400 rounded-full'
+          type="submit"
+        >
+          Sign Up
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
