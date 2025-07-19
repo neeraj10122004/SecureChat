6eef59e62afe0456b828dc4542b721f6e42e776b
neeraj10122004 <neerajsaiteja@gmail.com>
2025-07-19 10:11:42 +0530

    basic frontend

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import assets, { messagesDummyData } from '../assets/assets';
-
-export const ChatContainer = ({ className, selecteduser }) => {
+import { useEffect } from 'react';
+import { useRef } from 'react';
+export const ChatContainer = ({ className, selecteduser,setselecteduser }) => {
+  const scrollend = useRef();
+  useEffect(() => {
+    if(scrollend.current){
+        scrollend.current.scrollIntoView({ behavior : "smooth" })
+    }
+  }, [selecteduser])
+  
   return (
     <div className={`${className} h-full`}>
       {selecteduser ? (
@@ -37,11 +45,20 @@ export const ChatContainer = ({ className, selecteduser }) => {
                 <div className={`flex flex-row gap-1 ${
                   message.senderId === selecteduser._id ? 'flex-row-reverse' : ''
                 }`}>
+                  <div>
                   <img
-                    src={message.image}
+                    src={assets.avatar_icon}
                     alt={message._id}
                     className="w-8 h-8 rounded-full"
                   />
+                  <div>
+                    {new Date(message.createdAt).toLocaleTimeString("en-US", {
+hour: "2-digit",
+minute: "2-digit",
+hour12: false,
+})}
+                  </div>
+                  </div>
                   <div className="bg-white text-black rounded-md p-2 max-w-xs">
 
                     {message.image? <img src={message.image} alt="img" className='rounded-md max-w-60 max-h-60'/> : message.text}
@@ -50,7 +67,29 @@ export const ChatContainer = ({ className, selecteduser }) => {
                 </div>
               </div>
             ))}
+            <div ref={scrollend}></div>
           </div>
+          <div className="flex w-full gap-2 items-center p-2">
+          <div className="flex flex-grow bg-[#282142] rounded-full m-1 px-4 py-2 items-center">
+          < textarea
+            placeholder="Enter Message"
+            rows={1}
+            className="w-full bg-transparent text-white placeholder-gray-400 resize-none outline-none"
+          />
+          <input type="file" id='image' accept='image/png, image/jpeg' hidden/>
+          <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" className="w-5 mr-2
+            cursor-pointer"/>
+        </label>
+          </div>
+          <img
+          src={assets.send_button}
+          alt="send"
+          className="max-w-10 max-h-10 px-1 cursor-pointer"
+          />
+</div>
+
+          
         </>
       ) : (
         <div className="flex w-full h-full justify-center items-center">
--- frontend/src/components/RightSideBar.jsx
+++ frontend/src/components/RightSideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { imagesDummyData } from '../assets/assets';
 export const RightSideBar = ({className,selecteduser,setselecteduser}) => {
   
   if (!selecteduser) return null ;
@@ -11,6 +11,22 @@ export const RightSideBar = ({className,selecteduser,setselecteduser}) => {
                 <h1>{selecteduser.fullName}</h1>
                 <p className='text-green-500'>online</p>
               </div>
+          </div>
+          <hr className='my-4'/>
+          <div className='flex-col justify-center items-center text-xs'>
+            <p className='flex items-center justify-center py-3'>Media</p>
+            <div className='max-h-[170px] overflow-y-scroll grid grid-cols-2 gap-2'>
+              {imagesDummyData.map((url, key) => {
+                return (
+                  <img key={key} src={url} alt="img" onClick={() => window.open(url)} className='cursor-pointer' />
+                );
+              })}
+
+            </div>
+            
+          </div>
+          <div className='p-2'>
+              <button className='w-full p-1 bg-violet-500/50 rounded-full'>Logout</button>
           </div>
           
          
